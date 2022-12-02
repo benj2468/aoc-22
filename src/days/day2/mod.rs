99489a479f257a98d9b387a7eaca96a3ca0466fe
benjamin cape <benjcape@gmail.com>
2022-12-01 21:44:27 -0800

    oh boy more errors

--- src/days/day2/mod.rs
+++ src/days/day2/mod.rs
@@ -5,7 +5,7 @@ pub struct Line(u32, u32);
 
 impl Line {
     fn score(&self) -> u32 {
-        let score = if self.1 == ((self.0 + 1) % 3) {
+        let score = if self.1 == (self.0 + 1).rem_euclid(3) {
             6
         } else if self.1 == self.0 {
             3
@@ -19,20 +19,20 @@ impl Line {
 impl FromStr for Line {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let re = regex::Regex::new(r"([ABC]) ([XYZ])").unwrap();
+        let re = regex::Regex::new(r"([ABC]) ([XYZ])").map_err(|e| e.to_string())?;
         let mut groups = re.captures_iter(s);
-        let first = groups.next().unwrap();
+        let first = groups.next().ok_or("Bad Data")?;
         let op: i32 = match &first[1] {
             "A" => 0,
             "B" => 1,
             "C" => 2,
-            _ => panic!(),
+            _ => return Err("Bad Data".to_string()),
         };
         let me = match &first[2] {
             "X" => op - 1,
             "Y" => op,
             "Z" => op + 1,
-            _ => panic!(),
+            _ => return Err("Bad Data".to_string()),
         }
         .rem_euclid(3);
 
